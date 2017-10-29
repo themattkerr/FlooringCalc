59033ea49d19e7257d7f5fdc986ffaf23970f622
themattkerr <themattkerr@att.net>
2017-10-29 09:48:13 -0500

    stairstep check

--- floorsection.h
+++ floorsection.h
@@ -22,21 +22,24 @@ private:
     int m_nSectionWidth;
     int m_nBoardWidth;
     int m_nBoardLength;
-    int m_nNumberOfRows;
-    //int m_nNumOfFullBoardsPerRow;
+    int m_nNumberOfRows = 0;
+    int m_nNumberOfPreviousRows = 0;
     int m_nWaste;
     int m_nRemainderLength;
+
+
     int m_nSawBladeWidth = 2;
     int m_nExpansionGap = 8;
     int m_nMinimumLength = (9*16);
     int m_nNumOfRowsToCheck = 5;
     int m_nOverlapMin = (19*16);
+    int m_nStairStepRange = (2*16);
     int m_nCurrentCutNumber = 0;
 
-    //board m_sStarts[255];
+
     board m_sStarsBuffer[255];
     int m_nStartBufferIndex = 0;
-    //board m_sEnds[255];
+
     board m_sEndsBuffer[255];
     int m_nEndBufferIndex = 0;
     row m_sRow[255];
